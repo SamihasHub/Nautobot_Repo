991444892b6d4d60858bca692258ebe13277218f
SamihasHub <148326692+SamihasHub@users.noreply.github.com>
2026-04-28 01:23:00 +0600

    Register additional job reports in __init__.py

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,4 +1,12 @@
 from nautobot.apps.jobs import register_jobs
 from .device_report import ActiveDeviceReport
+from .ip_report import IPUtilizationReport
+from .circuit_report import CircuitStatusReport
+from .bts_report import BTSHealthReport
 
-register_jobs(ActiveDeviceReport)
+register_jobs(
+    ActiveDeviceReport,
+    IPUtilizationReport,
+    CircuitStatusReport,
+    BTSHealthReport,
+)
