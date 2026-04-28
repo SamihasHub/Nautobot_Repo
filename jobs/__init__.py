1e722fcf0e22a31ffa2309562ee78932eeb6ac98
SamihasHub <148326692+SamihasHub@users.noreply.github.com>
2026-04-28 14:43:54 +0600

    Add LiveDashboardGenerator to job registrations

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -3,10 +3,12 @@ from .device_report import ActiveDeviceReport
 from .ip_report import IPUtilizationReport
 from .circuit_report import CircuitStatusReport
 from .bts_report import BTSHealthReport
+from .live_dashboard import LiveDashboardGenerator
 
 register_jobs(
     ActiveDeviceReport,
     IPUtilizationReport,
     CircuitStatusReport,
     BTSHealthReport,
+    LiveDashboardGenerator,
 )
