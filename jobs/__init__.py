b83eb28322dceb5e0a4a33cd89ee4e3a54022272
SamihasHub <148326692+SamihasHub@users.noreply.github.com>
2026-04-28 14:40:02 +0600

    Update __init__.py

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -3,14 +3,10 @@ from .device_report import ActiveDeviceReport
 from .ip_report import IPUtilizationReport
 from .circuit_report import CircuitStatusReport
 from .bts_report import BTSHealthReport
-from .graph_report import GraphDashboardReport
-from .live_dashboard import LiveDashboardGenerator
 
 register_jobs(
     ActiveDeviceReport,
     IPUtilizationReport,
     CircuitStatusReport,
     BTSHealthReport,
-    GraphDashboardReport,
-    LiveDashboardGenerator,
 )
