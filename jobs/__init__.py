f656ee002057d409d33a77ef99efe3a63cc4cefe
SamihasHub <148326692+SamihasHub@users.noreply.github.com>
2026-04-28 14:35:06 +0600

    Update __init__.py

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,13 +1,16 @@
 from nautobot.apps.jobs import register_jobs
-
 from .device_report import ActiveDeviceReport
 from .ip_report import IPUtilizationReport
 from .circuit_report import CircuitStatusReport
 from .bts_report import BTSHealthReport
+from .graph_report import GraphDashboardReport
+from .live_dashboard import LiveDashboardGenerator
 
 register_jobs(
     ActiveDeviceReport,
     IPUtilizationReport,
     CircuitStatusReport,
-    BTSHealthReport
+    BTSHealthReport,
+    GraphDashboardReport,
+    LiveDashboardGenerator,
 )
